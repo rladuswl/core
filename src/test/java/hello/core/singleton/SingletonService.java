eaecfc0d9350865701217aed28580d86f962c52e
rladuswl <khkkgon18@naver.com>
2022-03-06 22:33:30 +0900

    싱글톤 패턴 적용하기 (스프링 컨테이너 사용 X)

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,21 @@
+package hello.core.singleton;
+
+// 싱글톤으로 만들기 - 해당 클래스 내부 말고는 SingletonService 생성 불가능
+public class SingletonService {
+
+    // 1. static 영역에 객체를 딱 1개만 생성 (instance)
+    private static final SingletonService instance = new SingletonService();
+
+    // 2. public으로 열어서 객체 인스턴스가 필요하면 이 static 메서드를 통해서만 조회하도록 허용 즉, instance를 꺼낼 수 있는 방법은 아래 메소드 뿐
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    // 3. 생성자를 private으로 선언하여 외부에서 new 키워드를 사용한 객체를 생성할 수 없게 막기
+    private SingletonService() {
+    }
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}
--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,14 +2,16 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonTest {
 
     @Test
-    @DisplayName("스프링 없는 순수한 DI 컨테이너") // 스프링이 없다? -> ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); 로 스프링 컨테이너를 만들지 않았다.
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+        // 스프링이 없다? -> ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); 로 스프링 컨테이너를 만들지 않았다.
     void pureContainer() {
         AppConfig appConfig = new AppConfig();
 
@@ -25,8 +27,26 @@ public class SingletonTest {
 
         // 테스트는 println 처럼 눈으로 확인하는 것 보다, 자동화 되도록 해야 한다.
         // memberService1 != memberService2 인 것을 검증하기
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    // 싱글톤을 구현한 SingletonService 클래스를 만든 후, 아래에서 테스트 해보기
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+
+        // 호출할 때마다 매번 새로운 객체를 생성할까? -> SingletonService에서 싱글톤 패턴을 적용했기에 항상 같은 객체 인스턴스가 호출됨
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // isEqualTo : (equals)
+        // isSameAs : (==) 참조어 비교
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
+
 
 
