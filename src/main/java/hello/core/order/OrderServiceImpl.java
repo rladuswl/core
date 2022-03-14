e2ca1fd1f87fa87f39f495ecdd39cbbafd724015
rladuswl <khkkgon18@naver.com>
2022-03-14 23:46:59 +0900

    lombok 라이브러리 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,22 +6,25 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final이 붙은 필드를 모아서 OrderServiceImpl 생성자를 자동으로 만들어줌
 public class OrderServiceImpl implements OrderService {
 
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy; // final은 값을 무조건 할당해야함->생성자 주입때 사용, 구현체에 의존하지 않고 역할(인터페이스)에만 의존하는 중
+    private final DiscountPolicy discountPolicy; // final은 값을 무조건 할당해야함 -> 생성자 주입때 사용, 구현체에 의존하지 않고 역할(인터페이스)에만 의존하는 중
 
-    @Autowired // 컴포넌트 스캔과 의존관계 자동 주입, 생성자가 딱 1개 있을 때는 @Autowired 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // @RequiredArgsConstructor 사용하여 아래 주석처리
+//    @Autowired // 컴포넌트 스캔과 의존관계 자동 주입, 생성자가 딱 1개 있을 때는 @Autowired 생략 가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order creatOrder(Long memberId, String itemName, int itemPrice) {
