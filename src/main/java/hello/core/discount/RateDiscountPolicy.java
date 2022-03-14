6d277f077c60455087b2bd9242d595dd55923aec
rladuswl <khkkgon18@naver.com>
2022-03-15 00:37:14 +0900

    조회 빈이 2개 이상인 문제 -> @Autowired 필드 명 매칭, @Qualifier, @Primary

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+//@Qualifier("mainDiscountPolicy") // 조회 대상 빈이 2개 이상일 때 해결 방법
 @Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,10 +8,11 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final이 붙은 필드를 모아서 OrderServiceImpl 생성자를 자동으로 만들어줌
+//@RequiredArgsConstructor // final이 붙은 필드를 모아서 OrderServiceImpl 생성자를 자동으로 만들어줌
 public class OrderServiceImpl implements OrderService {
 
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
@@ -20,11 +21,11 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy; // final은 값을 무조건 할당해야함 -> 생성자 주입때 사용, 구현체에 의존하지 않고 역할(인터페이스)에만 의존하는 중
 
     // @RequiredArgsConstructor 사용하여 아래 주석처리
-//    @Autowired // 컴포넌트 스캔과 의존관계 자동 주입, 생성자가 딱 1개 있을 때는 @Autowired 생략 가능
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired // 컴포넌트 스캔과 의존관계 자동 주입, 생성자가 딱 1개 있을 때는 @Autowired 생략 가능
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order creatOrder(Long memberId, String itemName, int itemPrice) {
